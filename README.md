817a1374cc368325530ed0d493196f2c6b0e7469
James Hodgkinson <james.hodgkinson@webcentralgroup.com.au>
2020-08-23 18:47:05 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+real simple, just iterates through the first-level subdirectories and does the equivalent of git status, only uglier.
---
+++ git_project_status.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+""" scans the local directory for sub-directories that have git repos in them, then does a git status on them """
+
+import os
+import json
+from loguru import logger
+from git import Repo
+import git.exc
+
+dirlist = os.listdir("./")
+
+def handle_diff(repo_object, compare=None, message="Changes"):
+    """ does the needful """
+    if repo_object.head.commit.diff(compare):
+        logger.info(f"{message}:")
+        for diff_added in repo.head.commit.diff(compare):
+            if diff_added.renamed:
+                logger.info(f"renamed : {diff_added.rename_from} -> {diff_added.rename_to}")
+            elif diff_added.new_file:
+                logger.info(f"new file: {diff_added.b_path}")
+            elif diff_added.change_type == 'M':
+                logger.info(f"modified: {diff_added.b_path}")
+            elif diff_added.change_type == 'D':
+                logger.info(f"deleted:  {diff_added.b_path}")
+            else:
+                logger.error(f"Unknown change type: '{diff_added.change_type}'")
+                logger.error(diff_added)
+                logger.debug(dir(diff_added))
+
+for filename in dirlist:
+    dirpath = f"./{filename}"
+    if os.path.isdir(dirpath):
+        logger.debug(f"Found dir: {dirpath}")
+        try:
+            repo = Repo(dirpath)
+        except git.exc.InvalidGitRepositoryError as error_message:
+            logger.debug(f"{dirpath} is not a repository, skipping ({error_message})")
+            continue
+        if repo.bare:
+            logger.info(f"{dirpath} is bare, ignoring.")
+            continue
+
+        if repo.is_dirty():
+            logger.warning(f"{dirpath} ({repo.active_branch}) dirty")
+            if repo.untracked_files:
+                logger.info("Untracked files:")
+                for filename in repo.untracked_files:
+                    logger.info(f"{filename}")
+
+            if repo.head.commit.diff('HEAD'):
+                logger.info()
+                for diff_added in repo.head.commit.diff('HEAD'):
+                    if diff_added.renamed:
+                        logger.info(f"renamed : {diff_added.rename_from} -> {diff_added.rename_to}")
+                    elif diff_added.new_file:
+                        logger.info(f"new file: {diff_added.b_path}")
+                    elif diff_added.change_type == 'M':
+                        logger.info(f"modified: {diff_added.b_path}")
+                    elif diff_added.change_type == 'D':
+                        logger.info(f"deleted:  {diff_added.b_path}")
+                    else:
+                        logger.error(f"Unknown change type: {diff_added.change_type}")
+                        logger.error(diff_added)
+                        logger.debug(dir(diff_added))
+            
+            handle_diff(repo, compare='HEAD', message='Changes staged for commit')
+            handle_diff(repo, compare=None, message='Changes not staged for commit')
+
+            logger.info("")
+            
+            #logger.debug(dir(repo))
+            #for branch in repo.branches:
+             #   logger.debug(dir(branch))
+            continue
